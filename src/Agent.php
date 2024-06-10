913fbbef7a1e32be61a455711b84267a5dbeec36
Muhammed Said Cakir <muhammedsaidckr@gmail.com>
2024-06-10 15:30:45 +0300

    Added new comment post reply feature

--- src/Agent.php
+++ src/Agent.php
@@ -20,10 +20,11 @@ class Agent
 
     public function __construct(
         public readonly User $user,
-        protected ?Client $client = null,
-        string $model = null,
-        int $maxTokens = null
-    ) {
+        protected ?Client    $client = null,
+        string               $model = null,
+        int                  $maxTokens = null
+    )
+    {
         $this->model = $model ?? 'gpt-3.5-turbo-instruct';
         $this->maxTokens = $maxTokens ?? 100;
     }
@@ -33,6 +34,67 @@ class Agent
         $content = $discussion->firstPost->content;
         $title = $discussion->title;
 
+        ['role' => $role, 'prompt' => $prompt] = $this->prepareChatForMessage();
+
+        $messages = $this->createMessages($title, $content, $role, $prompt);
+
+        $response = $this->sendCompletionRequest($messages);
+
+        if (empty($response->choices)) {
+            return;
+        }
+
+        $this->saveResponse($response, $discussion->id);
+    }
+
+    public function repliesToCommentPost(CommentPost $commentPost): void
+    {
+        // get the discussion title
+        $discussion = $commentPost->discussion;
+        $title = $discussion->title;
+        $content = $discussion->firstPost->content;
+
+        if (!$this->checkIfAssistantCanReplyToPost($commentPost)) {
+            return;
+        }
+
+        ['role' => $role, 'prompt' => $prompt] = $this->prepareChatForMessage();
+
+        $messages = $this->createMessages($title, $content, $role, $prompt);
+
+        $settings = resolve(SettingsRepositoryInterface::class);
+        $userPromptId = $settings->get('muhammedsaidckr-chatgpt.user_prompt');
+
+        // get the posts where the number is greater than 1 to the last message not include last message
+        $posts = $discussion->posts()
+            ->where('number', '>', 1)
+            ->where('number', '<', $commentPost->number)
+            ->get();
+
+        foreach ($posts as $post) {
+            if ($post->type == 'comment') {
+                $messages[] = [
+                    'role' => $post->user_id == $userPromptId ? 'assistant' : 'user',
+                    'content' => $post->content
+                ];
+            }
+        }
+
+        // add the last message with the prompt
+        $messages[] = $this->createMessageForUserWithPrompt($title, $commentPost->content, $prompt);
+
+        // answer to the post
+        $response = $this->sendCompletionRequest($messages);
+
+        if (empty($response->choices)) {
+            return;
+        }
+
+        $this->saveResponse($response, $discussion->id);
+    }
+
+    private function prepareChatForMessage(): array
+    {
         // get settings from the database
         $settings = resolve(SettingsRepositoryInterface::class);
         // get role
@@ -48,124 +110,95 @@ class Agent
             $prompt = 'Write a arguable or thankfully opinion asking or arguing something about an answer that has talked about "[title]" and who talked about [content]. Don\'t talk about what you would like or don\'t like. Speak in a close tone, like you are writing in a Tech Forum. Be random and unpredictable. Answer in [language].';
         }
 
-
-        // use chat method to reply to the discussion
-        $response = $this->client->chat()->create(
-            [
-                'model' => $this->model,
-                'messages' => [
-                    [
-                        'role' => 'system',
-                        'content' => $role
-                    ],
-                    [
-                        'role' => 'user',
-                        'content' => str_replace(
-                            ['[title]', '[content]'],
-                            [$title, $content],
-                            $prompt
-                        )
-                    ]
-                ],
-                'max_tokens' => $this->maxTokens
-            ]
-        );
-
-        if (empty($response->choices)) {
-            return;
-        }
-
-        $choice = Arr::first($response->choices);
-        $respond = $choice->message->content;
-
-        if (empty($respond)) {
-            return;
-        }
-
-        $userPrompt = $this->user->id;
-
-        CommentPost::reply(
-            discussionId: $discussion->id,
-            content: $respond,
-            userId: $userPrompt,
-            ipAddress: '127.0.0.1'
-        )->save();
+        return [
+            'role' => $role,
+            'prompt' => $prompt
+        ];
     }
 
-    public function repliesToCommentPost(CommentPost $commentPost)
+    private function sendCompletionRequest(array $messages)
     {
-        // get the discussion title
-        $discussion = $commentPost->discussion;
-        $title = $discussion->title;
-        $content = $discussion->firstPost->content;
-
-
-        $settings = resolve(SettingsRepositoryInterface::class);
-        $role = $settings->get('muhammedsaidckr-chatgpt.role');
-        if (empty($role)) {
-            // if the role is empty, set the role to default
-            $role = 'You are a helpful assistant.';
-        }
-
-        $userPromptId = $settings->get('muhammedsaidckr-chatgpt.user_prompt');
-
-        if ($commentPost->user_id == $userPromptId) {
-            return;
-        }
-
-        // is it the first post?
-        if ($commentPost->number == 1) {
-            return;
-        }
-        $messages = [];
-        $messages[] = ['role' => 'system', 'content' => $role];
-        $messages[] = [
-            'role' => 'user',
-            'content' => str_replace(
-                ['[title]', '[content]'],
-                [$title, $content],
-                $settings->get('muhammedsaidckr-chatgpt.prompt')
-            )
-        ];
-
-        foreach ($discussion->posts()->where('number', '>', 1)->get() as $post) {
-            if ($post->type == 'comment') {
-                $messages[] = [
-                    'role' => $post->user_id == $userPromptId ? 'assistant' : 'user',
-                    'content' => $post->content
-                ];
-            }
-        }
-
-        // log the messages
-        resolve('log')->info('Messages', $messages);
-
-        // answer to the post
-        $response = $this->client->chat()->create(
+        return $this->client->chat()->create(
             [
                 'model' => $this->model,
                 'messages' => $messages,
                 'max_tokens' => $this->maxTokens
             ]
         );
+    }
 
-        if (empty($response->choices)) {
-            return;
+
+    private function saveResponse($response, $discussionId): bool
+    {
+        try {
+            $choice = Arr::first($response->choices);
+            $respond = $choice->message->content;
+
+            if (empty($respond)) {
+                return false;
+            }
+
+            $userPrompt = $this->user->id;
+
+            CommentPost::reply(
+                discussionId: $discussionId,
+                content: $respond,
+                userId: $userPrompt,
+                ipAddress: '127.0.0.1'
+            )->save();
+
+            return true;
+        } catch (\Exception $e) {
+            resolve('log')->error($e->getMessage());
+            return false;
+        }
+    }
+
+    private function createMessages($title, $content, $role, $prompt): array
+    {
+        return [
+            ['role' => 'system', 'content' => $role],
+            $this->createMessageForUserWithPrompt($title, $content, $prompt)
+        ];
+    }
+
+    private function createMessageForUserWithPrompt($title, $content, $prompt): array
+    {
+        return [
+            'role' => 'user',
+            'content' => str_replace(
+                ['[title]', '[content]'],
+                [$title, $content],
+                $prompt
+            )
+        ];
+    }
+
+    private function checkIfAssistantCanReplyToPost($commentPost): bool
+    {
+        $discussion = $commentPost->discussion;
+
+        $settings = resolve(SettingsRepositoryInterface::class);
+
+        $userPromptId = $settings->get('muhammedsaidckr-chatgpt.user_prompt');
+        if ($commentPost->user_id == $userPromptId) {
+            return false;
         }
 
-        $choice = Arr::first($response->choices);
-        $respond = $choice->message->content;
-
-        if (empty($respond)) {
-            return;
+        // is it the first post?
+        if ($commentPost->number == 1) {
+            return false;
         }
 
-        CommentPost::reply(
-            discussionId: $discussion->id,
-            content: $respond,
-            userId: $userPromptId,
-            ipAddress: '127.0.0.1'
-        )->save();
+
+        $maxReplyCount = $settings->get('muhammedsaidckr-chatgpt.continue_to_reply_count');
+        $assistantReplyCount = $discussion->posts()->where('type', 'comment')->where('user_id', $userPromptId)->count();
+
+        if ($assistantReplyCount >= $maxReplyCount) {
+            return false;
+        }
+
+        return true;
     }
 
 }
